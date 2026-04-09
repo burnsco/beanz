0d361a6b242c47b17808f94d3c155912cc99f62e
Corey Burns <coreyburnsdev@aol.com>
2026-04-09 01:25:16 -0400

    chore: set production site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://beanz.coffee",
+  site: "https://beanz.coreyburns.ca",
   image: {
     remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
   },
